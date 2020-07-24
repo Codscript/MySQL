d07174ef89e388e6153d27d1c44959d6bc6a6de3
Codscript <mussa.monteiro@gmail.com>
2020-07-23 22:07:36 -0300

    Primero commit

---
+++ MySQL.sql
@@ -0,0 +1,12 @@
+-- create database cadastro;
+-- use cadastro;
+create table pessoas(
+nome varchar (30),
+idade tinyint,
+sexo varchar(2),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+
+describe pessoas;
